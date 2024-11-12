6908cd13d64ee470c3f03674c52ea9d538069d98
Dani Imdadur <dir131203@gmail.com>
2024-11-12 11:02:12 +0700

    first commit

---
+++ src/main/java/spring/security/practice/SpringSecurityApp.java
@@ -0,0 +1,13 @@
+package spring.security.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApp.class, args);
+    }
+
+}
---
+++ src/main/java/spring/security/practice/model/MyUserRepository.java
@@ -0,0 +1,10 @@
+package spring.security.practice.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface MyUserRepository extends JpaRepository<MyUser, Long> {
+    Optional<MyUser> findByUsername(String username);
+}
